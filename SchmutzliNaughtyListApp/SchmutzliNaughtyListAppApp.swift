6953d24f1b24608dc5e61a4d686b4a82bafba0b1
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-15 20:54:42 +0000

    Initial Commit

---
+++ SchmutzliNaughtyListApp/SchmutzliNaughtyListAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  SchmutzliNaughtyListAppApp.swift
+//  SchmutzliNaughtyListApp
+//
+//  Created by app-kaihatsusha on 15/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SchmutzliNaughtyListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
